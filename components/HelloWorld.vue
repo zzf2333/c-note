d6d43dc9b400ba1280b7bce4d83085e3b5e35da3
SaoNian <zr19911221@gmail.com>
2024-08-05 17:45:17 +0800

    Add ui

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,34 +1,39 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { Button } from "@/components/ui/button"
+import { ref } from "vue"
 
 defineProps({
-  msg: String,
-});
+    msg: String,
+})
 
-const count = ref(0);
+const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <Button>Button</Button>
+
+    <div class="card">
+        <button type="button" @click="count++">count is {{ count }}</button>
+        <p>
+            Edit
+            <code>components/HelloWorld.vue</code> to test HMR
+        </p>
+    </div>
+
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+        Install
+        <a href="https://github.com/vuejs/language-tools" target="_blank"
+            >Volar</a
+        >
+        in your IDE for a better DX
     </p>
-  </div>
-
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the WXT and Vue logos to learn more</p>
+    <p class="read-the-docs">Click on the WXT and Vue logos to learn more</p>
 </template>
 
 <style scoped>
 .read-the-docs {
-  color: #888;
+    color: #888;
 }
 </style>
